c7f6aa88945057fbd018c3efb065a1ba8f315ac7
Ali Faizan Kazmi <alifaizankazmi@gmail.com>
2019-11-02 22:30:14 +0000

    Refactor and simplify tests

--- src/Square.test.js
+++ src/Square.test.js
@@ -1,28 +1,30 @@
-import Square from './Square.js';
+import { assert } from 'chai';
 import { shallow } from 'enzyme';
-import { expect } from 'chai';
 import sinon from 'sinon';
+
+import Square from './Square.js';
 
 describe("Given a square with props", () => {
     let square;
     const onButtonClick = sinon.spy();
 
     beforeAll(() => {
-        square = Square({onClick: onButtonClick, value: "X"});
+        square = shallow(Square({
+            onClick: onButtonClick, 
+            value: "X"
+        }));
     });
 
-    it("it should be of type button", () => {
-        expect(square.type).to.equal("button")
+    it("it should consist of a single button", () => {
+        assert.equal(square.find('button').length, 1);
     });
 
     it("it should have an onClick function", () => {
-        const wrapper = shallow(square);
-        wrapper.find('button').simulate('click');
-        expect(onButtonClick).to.have.property('callCount', 1);
+        square.find('button').simulate('click');
+        assert(onButtonClick.calledOnce);
     });
 
     it("it should have a text value equal to its value prop", () => {
-        const wrapper = shallow(square);
-        expect(wrapper.find('button').text()).to.equal("X");
+        assert.equal(square.find('button').text(), "X");
     });
 });
